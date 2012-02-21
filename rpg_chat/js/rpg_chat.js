b41e5e1649b559990c7d05fffa2026f2d5483423
Sarah Hood <bastlynn@gmail.com>
2012-02-21 02:02:14 -0500

    Further refinements in rpg settings.

--- rpg_chat/js/rpg_chat.js
+++ rpg_chat/js/rpg_chat.js
@@ -8,27 +8,17 @@ Drupal.behaviors.vaxiaChat = {
   attach: function(context) { (function($) {
 
   var refeshRate = Drupal.settings.rpg_chat['refreshRate'];
-  var timer;
-  var chatRunning = 0;
 
   // On page load, start the chat running.
-  // Run only the one time to avoid ajax triggering multiple loads. 
-  $('document').once().ready(function() {
-    if (!chatRunning) {
-      chatRunning = 1;
-      refreshChat();
-    }
+  // Run only the one time to avoid ajax triggering multiple loads.
+  // On each ajax reload (as triggered by refreshChat) the ready is re-triggered.
+  $('document').ready(function() {
+    var timer = setTimeout(refreshChat, refeshRate * 1000);
   });
 
   // Function to refresh the chatroom by clicking submit.
   function refreshChat() {
-    $("#edit-refresh").triggerHandler("click");
-    clearTimeout(timer);
-    // TESTING.
-    // Since the refresh is driven by AJAX, the doc gets reloaded.
-    // When it's reloaded, the refresh hits up again.
-    // So we actually don't need to loop this.
-    //timer = setTimeout(refreshChat, refeshRate * 1000);
+    $("#edit-refresh-chat").triggerHandler("click");
   }
 
   })(jQuery); }
