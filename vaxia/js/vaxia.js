75b3c5e69859b22a6ef92b0aca1a5513ae5055f0
Sarah Hood <bastlynn@gmail.com>
2013-01-13 18:56:22 -0500

    Vaxia fixes on feedback rnd 2

--- vaxia/js/vaxia.js
+++ vaxia/js/vaxia.js
@@ -13,23 +13,23 @@ Drupal.behaviors.vaxiaChat = {
       'style="float:right;padding:0.5em 1em;margin:0.25em 4.4em;">' +
       'show/hide' +
       '</button>');
-    $('#mail #content').after('<button class="toggle-rpg-chat-sidebar button" ' +
-      'style="float:left;padding:0.5em 1em;margin:0.25em 4.4em;">' +
+    $('#main #content').after('<button class="toggle-rpg-chat-sidebar button" ' +
+      'style="float:left;margin:0.25em -20% 0.25em 0;">' +
       'show/hide' +
       '</button>');
+    $('#main .sidebars').css('margin-top', '40px');
   });
 
   // When Chat location toggle clicked, switch.
-  $('.toggle-rpg-chat-desc').click(function() {
-    $('#mail .sidebars').toggle();
-  });
-
-  // When Sidebar toggle clicked, switch.
   $('.toggle-rpg-chat-desc').click(function() {
     $('article.rpg-chat-node .field-name-field-artwork').toggle();
     $('article.rpg-chat-node .field-name-body').toggle();
     $('article.rpg-chat-node .field-name-field-parent').toggle();
     $('#block-views-rpg-chats-rpg-chats-children').toggle();
+  });
+  // When Sidebar toggle clicked, switch.
+  $('.toggle-rpg-chat-sidebar').click(function() {
+    $('#main .sidebars').toggle();
   });
 
   })(jQuery); }
