9430865d8210654652e142c6a067c9923a5a21a2
Sarah Hood <bastlynn@gmail.com>
2013-01-13 17:09:02 -0500

    Adding sidebar hide/show toggle

--- vaxia/js/vaxia.js
+++ vaxia/js/vaxia.js
@@ -10,12 +10,21 @@ Drupal.behaviors.vaxiaChat = {
   // Add toggle buttons.
   $('.tabs').once(function() {
     $('.tabs').after('<button class="toggle-rpg-chat-desc button" ' +
-      'style="float:right;padding:0.5em 1em;margin:0.25em;">' +
+      'style="float:right;padding:0.5em 1em;margin:0.25em 4.4em;">' +
+      'show/hide' +
+      '</button>');
+    $('#mail #content').after('<button class="toggle-rpg-chat-sidebar button" ' +
+      'style="float:left;padding:0.5em 1em;margin:0.25em 4.4em;">' +
       'show/hide' +
       '</button>');
   });
 
-  // When toggle clicked, switch.
+  // When Chat location toggle clicked, switch.
+  $('.toggle-rpg-chat-desc').click(function() {
+    $('#mail .sidebars').toggle();
+  });
+
+  // When Sidebar toggle clicked, switch.
   $('.toggle-rpg-chat-desc').click(function() {
     $('article.rpg-chat-node .field-name-field-artwork').toggle();
     $('article.rpg-chat-node .field-name-body').toggle();
