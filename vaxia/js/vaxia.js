70e2310b42cd37830fca846df2c433acb229b97e
Sarah Hood <bastlynn@gmail.com>
2013-01-13 22:59:54 -0500

    Vaxia fixes on feedback rnd 2

--- vaxia/js/vaxia.js
+++ vaxia/js/vaxia.js
@@ -18,25 +18,34 @@ Drupal.behaviors.vaxiaChat = {
       'show/hide' +
       '</button>');
     $('#main .sidebars, #main #content').css('margin-top', '40px');
-  });
 
-  // When Chat location toggle clicked, switch.
-  $('.toggle-rpg-chat-desc').click(function() {
-    $('article.rpg-chat-node .field-name-field-artwork').toggle();
-    $('article.rpg-chat-node .field-name-body').toggle();
-    $('article.rpg-chat-node .field-name-field-parent').toggle();
-    $('#block-views-rpg-chats-rpg-chats-children').toggle();
-  });
-  // When Sidebar toggle clicked, switch.
-  $('.toggle-rpg-chat-sidebar').click(function() {
-    $('#main .sidebars').toggle();
-    if ( $('#main .sidebars').is(':visible') ) {
-      $('#main #content').css('margin-left', '20%');
-      $('#main #content').css('width', '80%');
+
+    // When Chat location toggle clicked, switch.
+    $('.toggle-rpg-chat-desc').click(function() {
+    if ( $('article.rpg-chat-node .field-name-body').is(':visible') ) {
+      $('article.rpg-chat-node .field-name-field-artwork').hide();
+      $('article.rpg-chat-node .field-name-body').hide();
+      $('article.rpg-chat-node .field-name-field-parent').hide();
+      $('#block-views-rpg-chats-rpg-chats-children').hide();
     } else {
+      $('article.rpg-chat-node .field-name-field-artwork').show();
+      $('article.rpg-chat-node .field-name-body').show();
+      $('article.rpg-chat-node .field-name-field-parent').show();
+      $('#block-views-rpg-chats-rpg-chats-children').show();
+    }
+    });
+    // When Sidebar toggle clicked, switch.
+    $('.toggle-rpg-chat-sidebar').click(function() {
+    if ( $('#main .sidebars').is(':visible') ) {
+      $('#main .sidebars').hide();
       $('#main #content').css('margin-left', '0%');
       $('#main #content').css('width', '100%');
+    } else {
+      $('#main .sidebars').show();
+      $('#main #content').css('margin-left', '20%');
+      $('#main #content').css('width', '80%');
     }
+    });
   });
 
   })(jQuery); }
