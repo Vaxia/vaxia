d18a5d7ae46d89c26a99249b959cc138c2539581
Sarah Hood <bastlynn@gmail.com>
2013-01-14 14:51:46 -0500

    Adding custom select image for form display, aiming to make select dropdown visual

---
+++ vaxia/select_image/js/select_image.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Javascript for RPG chat UI.
+ *
+ * Refreshes the AJAX comments on a regular refresh.
+ */
+Drupal.behaviors.selectImage = {
+  attach: function(context) { (function($) {
+
+    // On ready, hook up the image dropdown code to supporting selects.
+    try {
+      $('body select.form-select-image').msDropDown();
+    } catch(e) {
+      alert(e.message);
+    }
+
+  })(jQuery); }
+}
