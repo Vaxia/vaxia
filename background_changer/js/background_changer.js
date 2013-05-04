17e396c5e58989b9dea1a6be082ab0c44e494c18
Andy Click <andyclick@gmail.com>
2013-05-04 14:27:53 -0300

    Update background_changer.js

--- background_changer/js/background_changer.js
+++ background_changer/js/background_changer.js
@@ -6,16 +6,23 @@
 
 Drupal.behaviors.backgroundChanger = {
   attach: function(context) { (function($) {
-    
-  function setBackground (d,n) {
+  
+  
+  function setBackground (d,n,path) {
+    //Get the current time on the user's end and capture the hours digit
     d = new Date();
     n = d.getHours();
-
+    
+    //Converting 24-hour clock to 12-hour, with PM hours working in reverse (1pm = 11am)
     if (n>12){
       n = 24-n;
     }
-
-    $('body').css('background-image','url(/sites/all/themes/vaxia_theme/vaxia/images/'+ n +'.jpg';
+    
+    //Find the image subfolder within the background_changer module
+    path = DRUPAL_ROOT + '/' + drupal_get_path('module', 'background_changer') + "/images/" + n + ".jpg";
+  
+    //Swap the background-image attribute on the body tag
+    $('body').css('background-image',path);
 
       }
     }
