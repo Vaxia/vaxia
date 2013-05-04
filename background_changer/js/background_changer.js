ab2bd3547481b81f3e11b0307ee49ff043e1c3c8
Andy Click <andyclick@gmail.com>
2013-05-04 08:13:44 -0300

    Update background_changer.js

--- background_changer/js/background_changer.js
+++ background_changer/js/background_changer.js
@@ -1,5 +1,17 @@
-function setBackground (d,n) {
-  d = new Date();
-  n = d.getHours();
-  $('body').css('background-image','url(/sites/all/themes/vaxia_theme/vaxia/images/'+ n +'.jpg';
+/**
+ * @file
+ * Javascript for body background-image.
+ * 
+ */
+
+Drupal.behaviors.backgroundChanger = {
+  attach: function(context) { (function($) {
+    
+  function setBackground (d,n) {
+    d = new Date();
+    n = d.getHours();
+    $('body').css('background-image','url(/sites/all/themes/vaxia_theme/vaxia/images/'+ n +'.jpg';
+      }
+    }
+  }
 }
