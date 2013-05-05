e90bb692cba5a6a1d0c8c98a1d32aa08af3be1b1
Sarah Hood <bastlynn@gmail.com>
2013-05-05 11:41:15 -0400

    Corrected loads of js errs and a pathing err

--- background_changer/js/background_changer.js
+++ background_changer/js/background_changer.js
@@ -1,31 +1,26 @@
 /**
  * @file
  * Javascript for body background-image.
- * 
+ *
  */
 
 Drupal.behaviors.backgroundChanger = {
   attach: function(context) { (function($) {
-  
-  
-  function setBackground (d,n,path) {
-    //Get the current time on the user's end and capture the hours digit
+
+  function setBackground(path) {
+    // Get the current time on the user's end and capture the hours digit
     d = new Date();
     n = d.getHours();
-    
-    //Converting 24-hour clock to 12-hour, with PM hours working in reverse (1pm = 11am)
-    if (n>12){
-      n = 24-n;
+    // Converting 24-hour clock to 12-hour, with PM hours working in reverse (1pm = 11am)
+    if (n > 12){
+      n = 24 - n;
     }
-    
-    //Grab the base path from Drupal.settings and add the specific background image
-    var path = Drupal.settings.background-changer.path;
-    path = path + n + ".jpg";
-  
-    //Swap the background-image attribute on the body tag
-    $('body').css('background-image',path);
-
-      }
-    }
+    // Grab the base path from Drupal.settings and add the specific background image
+    path = path + n + '.jpg';
+    // Swap the background-image attribute on the body tag
+    $('body').css('background-image', 'url(' + path + ')');
   }
+  setBackground(Drupal.settings.background_changer.path);
+
+  })(jQuery); }
 }
