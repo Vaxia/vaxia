9a4d418ef07d6470f8cb01a8f674758b9de842bf
Sarah Hood <bastlynn@gmail.com>
2013-05-13 14:03:25 -0400

    Add cookie based sticky chars and colors

--- dice_helper/js/dice_helper.js
+++ dice_helper/js/dice_helper.js
@@ -8,6 +8,27 @@
 Drupal.behaviors.diceHelper = {
   attach: function(context) { (function($) {
 
+  // Cookie settings.
+  function setCookie(c_name, value, exdays) {
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+  }
+
+  function getCookie(c_name) {
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++) {
+      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      x=x.replace(/^\s+|\s+$/g,"");
+      if (x==c_name){
+        return unescape(y);
+      }
+    }
+    return 0;
+  }
+
   // Respond to button clicks.
   function quickPick(type) {
     $('#edit-vaxia-rolls-dice-0-number').val(1);
@@ -63,6 +84,19 @@ Drupal.behaviors.diceHelper = {
     }
   }
 
+  // Set the value in the form based on the given character index.
+  function setCharacterColorNPic(character_value) {
+    // Check the cookie against the helper value to set pic and color.
+    var color = getCookie('vaxia_dice_helper_' + character_value + '_color');
+    if (typeof color !== 'undefined') {
+      $('#edit-field-comment-color-und-0-value').val(color);
+    }
+    var pic = getCookie('vaxia_dice_helper_' + character_value + '_pic');
+    if (typeof pic !== 'undefined') {
+      $('body select.form-select-image').msDropDown().data('dd').setIndexByValue(pic);
+    }
+  }
+
   // Add toggle buttons, but only the once.
   $('#vaxia-dice-roller').once(function() {
     helper = $('#dice-helper');
@@ -84,6 +118,7 @@ Drupal.behaviors.diceHelper = {
       else {
         $('#dice-helper').show();
       }
+      setCharacterColorNPic(helper);
     }
   });
 
@@ -96,6 +131,16 @@ Drupal.behaviors.diceHelper = {
     else {
       $('#dice-helper').show();
     }
+    setCharacterColorNPic(helper);
+  });
+
+  // Save the color and pic on post.
+  $('#edit-submit, #edit-1').click(function() {
+    var helper = $('#edit-field-comment-character-und').val();
+    var color = $('#edit-field-comment-color-und-0-value').val();
+    setCookie('vaxia_dice_helper_' + helper + '_color', color, 30);
+    var pic = $('#edit-field-artwork-und').val();
+    setCookie('vaxia_dice_helper_' + helper + '_pic', pic, 30);
   });
 
   $('.dice-helper-button').click(function() {
