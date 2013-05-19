31f0896094c5dbda38dc311e2e4a1a7d3fe4bf7c
Sarah Hood <bastlynn@gmail.com>
2013-05-19 13:22:59 -0400

    Correct hook name, and add suggested type for node type templates

--- vaxia_theme/template.php
+++ vaxia_theme/template.php
@@ -111,7 +111,7 @@
  *   The name of the template being rendered ("maintenance_page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_maintenance_page(&$variables, $hook) {
+function vaxia_theme_preprocess_maintenance_page(&$variables, $hook) {
   // When a variable is manipulated or added in preprocess_html or
   // preprocess_page, that same work is probably needed for the maintenance page
   // as well, so we can just re-use those functions to do that work here.
@@ -129,7 +129,7 @@ function eclipse_preprocess_maintenance_page(&$variables, $hook) {
  *   The name of the template being rendered ("html" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_html(&$variables, $hook) {
+function vaxia_theme_preprocess_html(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // The body tag's classes are controlled by the $classes_array variable. To
@@ -147,7 +147,7 @@ function eclipse_preprocess_html(&$variables, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_page(&$variables, $hook) {
+function vaxia_theme_preprocess_page(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -160,16 +160,9 @@ function eclipse_preprocess_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("node" in this case.)
  */
-/* -- Delete this line if you want to use this function
-function eclipse_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // Optionally, run node-type-specific preprocess functions, like
-  // eclipse_preprocess_node_page() or eclipse_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $variables['node']->type;
-  if (function_exists($function)) {
-    $function($variables, $hook);
-  }
+function vaxia_theme_preprocess_node(&$variables, $hook) {
+  $node = $variables['node'];
+  $variables['theme_hook_suggestions'][] = 'node__' . $node->type . '_' . $variables['elements']['#view_mode'];
 }
 // */
 
@@ -182,7 +175,7 @@ function eclipse_preprocess_node(&$variables, $hook) {
  *   The name of the template being rendered ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_comment(&$variables, $hook) {
+function vaxia_theme_preprocess_comment(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -196,7 +189,7 @@ function eclipse_preprocess_comment(&$variables, $hook) {
  *   The name of the template being rendered ("region" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_region(&$variables, $hook) {
+function vaxia_theme_preprocess_region(&$variables, $hook) {
   // Don't use Zen's region--sidebar.tpl.php template for sidebars.
   //if (strpos($variables['region'], 'sidebar_') === 0) {
   //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
@@ -213,7 +206,7 @@ function eclipse_preprocess_region(&$variables, $hook) {
  *   The name of the template being rendered ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function eclipse_preprocess_block(&$variables, $hook) {
+function vaxia_theme_preprocess_block(&$variables, $hook) {
   // Add a count to all the blocks in the region.
   // $variables['classes_array'][] = 'count-' . $variables['block_id'];
 
