b524b5c38f1506f968c53e2ca972b9db72d422e8
Sarah Hood <bastlynn@gmail.com>
2014-01-11 18:14:56 -0500

    Add pause button and move ajax protections into scripts

--- vaxia/rpg_chat/js/rpg_chat.js
+++ vaxia/rpg_chat/js/rpg_chat.js
@@ -8,23 +8,75 @@ Drupal.behaviors.rpgChat = {
   attach: function(context) { (function($) {
 
   // Grab the refresh rate from the user settings.
-  var refeshRate = Drupal.settings.rpg_chat['refreshRate'];
+  var refreshRate = Drupal.settings.rpg_chat['refreshRate'];
+  var paused = false;
+  var timer;
 
   // Function to refresh the chatroom by clicking submit.
   function refreshChat() {
-    $("#edit-refresh-chat").triggerHandler("click");
+    if (paused == false) {
+      $("#edit-refresh-chat").triggerHandler("click");
+    }
+    else {
+      timer = setTimeout(refreshChat, refreshRate * 1000);
+    }
   }
+
+  // Once, on page load, add the pause button to the interface.
+  $('document').ready(function() {
+    if (!$('.rpg-chat-node #edit-pause').length) {
+      // On page load, inject the buttons into place in the DOM.
+      $('.rpg-chat-node #edit-actions').prepend('<input type="submit" id="edit-pause" class="toggle-rpg-chat-pause form-submit" value="Pause">');
+      $('.rpg-chat-node #edit-0').before('<input type="submit" id="edit-2" class="toggle-rpg-chat-pause form-submit" value="Pause">');
+      $('.toggle-rpg-chat-pause').css('font-weight', '').css('color', 'graytext');
+    }
+  });
+
+  // When clicking the comment post, empty the post out.
+  $('.toggle-rpg-chat-pause').click(function() {
+    if (paused == false) {
+      paused = true;
+      $('.toggle-rpg-chat-pause').css('font-weight', 'bold').css('color', 'red');
+    }
+    else {
+      paused = false;
+      $('.toggle-rpg-chat-pause').css('font-weight', '').css('color', 'graytext');
+    }
+    // Otherwise do nothing.
+    return false;
+  });
+
+  // Prevent form submission while in an AJAX event.
+  $('form.node-form, form.comment-form').ajaxStart(function(){
+    $(this).submit(function() {
+      return false;
+    });
+  })
+  .ajaxStop(function() {
+    $(this).unbind('submit');
+    $(this).submit(function() {
+      return true;
+    });
+  });
+
+  // Prevent anchor clicks while in an AJAX event.
+  $('a').ajaxStart(function(){
+    $(this).click(function() {
+      return false;
+    });
+  })
+  .ajaxStop(function() {
+    $(this).unbind('click');
+    $(this).click(function() {
+      return true;
+    });
+  });
 
   // On page load, start the chat running.
   // Run only the one time to avoid ajax triggering multiple loads.
   // On each ajax reload (as triggered by refreshChat) the ready is re-triggered.
   $('document').ready(function() {
-    var timer = setTimeout(refreshChat, refeshRate * 1000);
-  });
-
-  // When clicking the comment post, empty the post out.
-  $('#edit-submit').ajaxComplete(function() {
-    $('#comment-form #edit-comment-body-und-0-value, #comment-form #edit-vaxia-rolls-notes').val();
+    timer = setTimeout(refreshChat, refreshRate * 1000);
   });
 
   })(jQuery); }
