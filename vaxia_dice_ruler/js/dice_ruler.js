7630de3e0f526440b0757369e71d1fbdbd1f9385
Sarah Hood <bastlynn@gmail.com>
2014-01-18 02:28:09 -0500

    Add JS to dice ruler to pull data into ruler if can be guessed at type

---
+++ vaxia_dice_ruler/js/dice_ruler.js
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * Javascript for RPG chat UI.
+ *
+ * Refreshes the AJAX comments on a regular refresh.
+ */
+Drupal.behaviors.diceRuler = {
+  attach: function(context) { (function($) {
+
+  // Once, on page load, add the pause button to the interface.
+  $('document').ready(function() {
+    // On page load, inject the button into place in the DOM.
+    var rules = $('.dice_rule').length;
+    if (rules == 0) {
+      var rule_link = '<div class="dice_rule"><a href="#">*</a></div>';
+      $('.suggested_rule').before(rule_link);
+    }
+  });
+
+  // And load it all up when clicked on.
+  $('.dice_rule').click(function() {
+    // On page load, inject the button into place in the DOM.
+    var rolls = $(this).parent('.dice_rolls');
+    var suggested = rolls.find('.suggested_rule').html();
+    switch(suggested) {
+      case 'one-trait':
+        $('#edit-roll-type').val('one_trait').trigger('change');
+        $('#edit-one-trait-might').val( rolls.find('.dice_roll_0 .might').html() );
+        $('#edit-one-trait-rolled').val( rolls.find('.dice_roll_0 .roll').html() );
+      break;
+      case 'two-trait':
+        $('#edit-roll-type').val('two_trait').trigger('change');
+        $('#edit-two-trait-might-a').val( rolls.find('.dice_roll_0 .might').html() );
+        $('#edit-two-trait-rolled-a').val( rolls.find('.dice_roll_0 .roll').html() );
+        $('#edit-two-trait-might-b').val( rolls.find('.dice_roll_1 .might').html() );
+        $('#edit-two-trait-rolled-b').val( rolls.find('.dice_roll_1 .roll').html() );
+      break;
+      case 'magic':
+        $('#edit-roll-type').val('magic').trigger('change');
+        $('#edit-magic-might-a').val( rolls.find('.dice_roll_int .might').html() );
+        $('#edit-magic-rolled-a').val( rolls.find('.dice_roll_int .roll').html() );
+        $('#edit-magic-might-b').val( rolls.find('.dice_roll_spi .might').html() );
+        $('#edit-magic-rolled-b').val( rolls.find('.dice_roll_spi .roll').html() );
+      break;
+      case 'combat':
+        $('#edit-roll-type').val('combat').trigger('change');
+        $('#edit-combat-might-a-dex').val( rolls.find('.dice_roll_dex .might').html() );
+        $('#edit-combat-rolled-a-dex').val( rolls.find('.dice_roll_dex .roll').html() );
+        $('#edit-combat-might-a-str').val( rolls.find('.dice_roll_str .might').html() );
+        $('#edit-combat-rolled-a-str').val( rolls.find('.dice_roll_str .roll').html() );
+        $('#edit-combat-might-a-end').val( rolls.find('.dice_roll_end .might').html() );
+        $('#edit-combat-rolled-a-end').val( rolls.find('.dice_roll_end .roll').html() );
+      break;
+    }
+    return false; // Don't go anywhere.
+  });
+
+  })(jQuery); }
+}
--- vaxia_theme/templates/dice-roller-results.tpl.php
+++ vaxia_theme/templates/dice-roller-results.tpl.php
@@ -12,36 +12,79 @@
 <?php
   $str_rolls = '';
   if (isset($dice_rolls) && is_array($dice_rolls)) {
+    $stats_involved = array();
+    $stats_rolled = '';
     if (isset($dice_rolls[0]['roll_notes'])) {
+      // Notes starts as a list of all rolls and results, separated by "<br>".
       $notes = explode('<br>', $dice_rolls[0]['roll_notes']);
+      // An array of each note, in the format: array('0' => "1 : roll(1d100) + agi (26)");
       foreach ($dice_rolls as $index => $dice_roll) {
         $note = '';
+        // Trim "1 : roll" off the note. New format: roll(1d100) + agi (26)
         if (isset($notes[$index + 1])) {
           $note = substr($notes[$index + 1], 8);
+          // We have a note, let's get the stat.
+          $results = explode('+', $note);
+          $results = explode(' ', trim($results[1]));
+          $stat = trim($results[0]);
+          $stats_involved[] = $stat;
         }
-        $results = explode(',', $dice_rolls[$index]['roll_result']);
-        $might = array_pop($results);
-        $might = explode('=', $might);
-        $might = trim($might[0]);
-        $roll = implode(', ', $results);
-        if (!empty($roll)) {
-          $str_rolls .= '<div class="dice">' .
-            t('!note => <b>Roll:</b> @roll, <b>Might:</b> @might',
-              array('!note' => $note, '@command' => $dice_rolls[$index]['roll_command'],
-              '@roll' => $roll,
-              '@might' => $might)
-            ) .
-            '</div>'."\n";
+        // The roll and the might are pulled out of the roll results.
+        // That starts as a format: 28, 26 = 54, everything after the "=" can be ignored.
+        // Since that's not how the Vaxian system works.
+        $results = explode('=', $dice_rolls[$index]['roll_result']);
+        // Drop everything after the "=" on the floor. We don't need it.
+        $results = trim($results[0]);
+        // New format "66, 25, 22, 99". Where 99 is the might.
+        $results =  explode(',', $results);
+        // The the first rolls are the rolls. The very last one is the might.
+        // If there is no might, leave it empty. For 1-dice rolls.
+        $might = count($results) > 1 ? trim(array_pop($results)) : '';
+        $rolls = trim(implode(', ', $results));
+        if (!empty($might)) {
+          $str_rolls .= '<div class="dice">' . $note . ' => ' .
+            '<b>' . t('Roll') . ':</b> ' . $rolls . ' <b>' . t('Might') . ':</b> ' . $might . '</div>' . "\n";
+            // Add the hidden div for jQuery to tie into for the ruler.
+            // All hidden, so no need for text translation here.
+          $rolls = explode(',', $rolls);
+          foreach ($rolls as $roll) {
+            $stats_rolled .=  '  <div class="dice_roll dice_roll_' . $stat . ' dice_roll_' . $index . '" style="display:none;">' . "\n" .
+              '    <div class="stat">' . $stat . '</div>' . "\n" .
+              '    <div class="roll">' . trim($roll) . '</div>' . "\n" .
+              '    <div class="might">' . $might . '</div>' . "\n" .
+              '  </div>'. "\n" ;
+          }
         }
         else {
-          $str_rolls .= '<div class="dice">' .
-            t('!note => <b>Roll:</b> @roll',
-              array('!note' => $note, '@command' => $dice_rolls[$index]['roll_command'],
-              '@roll' => $might)
-            ) .
-            '</div>'."\n";
+          $str_rolls .= '<div class="dice">' . $note . ' => <b>' . t('Roll') . ':</b> ' . $roll . '</div>' . "\n";
         }
+      } // End loop processing this roll.
+    } // End if protecting the roll set.
+    // We now know all the stats involved in this roll.
+    $roll_type = 'unknown';
+    if (count($stats_involved) == 1) {
+      $roll_type = 'one-trait';
+    }
+    if (count($stats_involved) == 2) {
+      $roll_type = 'two-trait';
+      $magic_stats = array('int', 'spi');
+      $diffs = array_diff($stats_involved, $magic_stats);
+      if (empty($diffs)) {
+        $roll_type = 'magic';
       }
+    }
+    if (count($stats_involved) == 3) {
+      $magic_stats = array('dex', 'str', 'end');
+      $diffs = array_diff($stats_involved, $magic_stats);
+      if (empty($diffs)) {
+        $roll_type = 'combat';
+      }
+    }
+    if ($roll_type != 'unknown') {
+      $str_rolls .= '<div class="dice_rolls">'. "\n"  .
+        '  <div class="suggested_rule" style="display:none;">' . $roll_type . '</div>'. "\n"  .
+        $stats_rolled. "\n"  .
+        '</div>'. "\n";
     }
   }
   print $str_rolls;
