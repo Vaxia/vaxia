879e6be1062e02922c25e76b337dc6c1e046f929
Sarah Hood <bastlynn@gmail.com>
2014-02-23 11:53:13 -0500

    Only do the new UI on chatrooms

--- vaxia_theme/vaxia/js/vaxia_pullouts.js
+++ vaxia_theme/vaxia/js/vaxia_pullouts.js
@@ -3,9 +3,9 @@
   Drupal.behaviors.themePullouts = {
     attach: function (context, settings) {
 
-
   // Add toggle buttons.
-  $('.tabs').once(function() {
+  $('.logged-in article.node-rpg-chatroom').once(function() {
+
     // Setup.
     $('.sidebar-first #content').css('margin-left', '0%');
     $('.sidebar-first #content').css('width', '100%');
@@ -13,22 +13,23 @@
     $('#header').hide();
 
     // Pullout for Header.
-    $('#navigation').wrap('<div id="extruderBarHead" class="{title:\'View Header\'}"></div>');
+    $('#navigation').wrap('<div id="extruderBarHead" class="{title:\'View Header\'} isClosed"></div>');
     $('#extruderBarHead').buildMbExtruder({
       position:"left",
-      width:"100%",
       positionFixed:false,
       top:60,
       extruderOpacity:0.8,
       onExtOpen:function(){
-        $('#extruderBarHead').toggleClass('isOpen');
+        $('#extruderBarHead').addClass('isOpen');
+        $('#extruderBarHead').removeClass('isClosed');
         $('#header').show();
         $('.sidebar-first #content').css('margin-left', '20%');
         $('.sidebar-first #content').css('width', '80%');
       },
       onExtContentLoad:function(){},
       onExtClose:function(){
-        $('#extruderBarHead').toggleClass('isOpen');
+        $('#extruderBarHead').removeClass('isOpen');
+        $('#extruderBarHead').addClass('isClosed');
         $('#header').hide();
         $('.sidebar-first #content').css('margin-left', '0%');
         $('.sidebar-first #content').css('width', '100%');
@@ -36,59 +37,62 @@
     });
 
     // Pullout for Sidebar.
-    $('.sidebar-first .region-sidebar-first').wrap('<div id="extruderBarFirst" class="{title:\'View Sidebar\'}"></div>');
+    $('.sidebar-first .region-sidebar-first').wrap('<div id="extruderBarFirst" class="{title:\'View Sidebar\'} isClosed"></div>');
     $('#extruderBarFirst').buildMbExtruder({
       position:"left",
-      width:"100%",
       positionFixed:false,
       top:170,
       extruderOpacity:0.8,
       onExtOpen:function(){
-        $('#extruderBarFirst').toggleClass('isOpen');
+        $('#extruderBarFirst').addClass('isOpen');
+        $('#extruderBarHead').removeClass('isClosed');
         $('.sidebar-first #content').css('margin-left', '20%');
         $('.sidebar-first #content').css('width', '80%');
       },
       onExtContentLoad:function(){},
       onExtClose:function(){
-        $('#extruderBarFirst').toggleClass('isOpen');
+        $('#extruderBarFirst').removeClass('isOpen');
+        $('#extruderBarHead').addClass('isClosed');
         $('.sidebar-first #content').css('margin-left', '0%');
         $('.sidebar-first #content').css('width', '100%');
       }
     });
 
     // Pullout for Description.
-    $('article.node-rpg-chatroom .field-name-field-wiki-source').wrap('<div id="extruderBarDesc" class="{title:\'View Description\'}"></div>');
+    $('article.node-rpg-chatroom .field-name-field-wiki-source').wrap('<div id="extruderBarDesc" class="{title:\'View Description\'} isClosed"></div>');
     $('#extruderBarDesc').buildMbExtruder({
       position:"left",
-      width:"100%",
       positionFixed:false,
       top:283,
       extruderOpacity:0.8,
       onExtOpen:function(){
-        $('#extruderBarDesc').toggleClass('isOpen');
+        $('#extruderBarDesc').addClass('isOpen');
+        $('#extruderBarDesc').removeClass('isClosed');
       },
       onExtContentLoad:function(){},
       onExtClose:function(){
-        $('#extruderBarDesc').toggleClass('isOpen');
+        $('#extruderBarDesc').removeClass('isOpen');
+        $('#extruderBarDesc').addClass('isClosed');
       }
     });
 
     // Pullout for Room Navigation.
-    $('article.node-rpg-chatroom #node_rpg_chatroom_full_group_navigation').wrap('<div id="extruderBarNav" class="{title:\'View Navigation\'}"></div>');
+    $('article.node-rpg-chatroom #node_rpg_chatroom_full_group_navigation').wrap('<div id="extruderBarNav" class="{title:\'View Room Nav\'} isClosed"></div>');
     $('#extruderBarNav').buildMbExtruder({
       position:"left",
-      width:"100%",
       positionFixed:false,
       top:417,
       extruderOpacity:0.8,
       onExtOpen:function(){
-        $('#extruderBarNav').toggleClass('isOpen');
+        $('#extruderBarNav').addClass('isOpen');
+        $('#extruderBarDesc').removeClass('isClosed');
         $('.sidebar-first #content').css('margin-left', '20%');
         $('.sidebar-first #content').css('width', '80%');
       },
       onExtContentLoad:function(){},
       onExtClose:function(){
-        $('#extruderBarNav').toggleClass('isOpen');
+        $('#extruderBarNav').removeClass('isOpen');
+        $('#extruderBarHead').addClass('isClosed');
         $('.sidebar-first #content').css('margin-left', '0%');
         $('.sidebar-first #content').css('width', '100%');
       }
