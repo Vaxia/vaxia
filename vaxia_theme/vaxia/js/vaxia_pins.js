2614d2a7338fe83281d841265607325a91b2cc81
Sarah Hood <bastlynn@gmail.com>
2014-03-09 11:35:12 -0500

    Fix JS issue with pins scrpit

--- vaxia_theme/vaxia/js/vaxia_pins.js
+++ vaxia_theme/vaxia/js/vaxia_pins.js
@@ -3,16 +3,24 @@
 
   Drupal.behaviors.pinDisplay = {
     attach: function (context, settings) {
-    t = setTimeout( function() {
-      $('.pinterest_display').children('span').addClass('pinterest_embed_grid');
-      $('.pinterest_embed_grid').children('span').addClass('pinterest_embed_grid_span');
-      $('.pinterest_embed_grid_span').filter(":even").addClass('pinterest_embed_grid_hd');
-      $('.pinterest_embed_grid_span').filter(":odd").addClass('pinterest_embed_grid_bd');
-      $('.pinterest_display span span span a').addClass('pin_link');
-      $('.pinterest_display span span span a img').addClass('pin_image');
-      $('.pin_image').attr('height', '').attr('width', '').attr('style', '');
-      $('.pin_link').attr('height', '').attr('width', '').attr('style', '');
-    }, 3000);
-  };
+      // Declare function.
+      function addCssToPins() {
+        $('.pinterest_display').children('span').addClass('pinterest_embed_grid');
+        $('.pinterest_embed_grid').children('span').addClass('pinterest_embed_grid_span');
+        $('.pinterest_embed_grid_span').filter(":even").addClass('pinterest_embed_grid_hd');
+        $('.pinterest_embed_grid_span').filter(":odd").addClass('pinterest_embed_grid_bd');
+        $('.pinterest_display span span span a').addClass('pin_link');
+        $('.pinterest_display span span span a img').addClass('pin_image');
+        $('.pin_image').attr('height', '').attr('width', '').attr('style', '');
+        $('.pin_link').attr('height', '').attr('width', '').attr('style', '');
+      }
 
+      // Run it when the page is done.
+      $('document').ready(function() {
+        $('.pinterest_display').once(function() {
+           t = setTimeout(addCssToPins, 3000);
+        });
+      });
+
+  };
 })(jQuery);
