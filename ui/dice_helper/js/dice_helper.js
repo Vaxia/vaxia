6e4c99ec5b4902a8e7ed0254df1bc44d81e64872
Bastlynn <bastlynn@gmail.com>
2015-04-16 04:31:57 -0400

    ajaxComplete

--- ui/dice_helper/js/dice_helper.js
+++ ui/dice_helper/js/dice_helper.js
@@ -171,7 +171,7 @@ Drupal.behaviors.diceHelper = {
   });
 
     //  One time bindings, changing the character pic dropdown.
-    $('#vaxia-dice-roller').ready(function() {
+    $('#vaxia-dice-roller').ajaxComplete(function() {
       setDiceHelper();
     });
     
