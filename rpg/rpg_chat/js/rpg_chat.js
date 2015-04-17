2abb33b079c88c4e473a52ee9849d93a524ca36f
Bastlynn <bastlynn@gmail.com>
2015-04-16 20:50:39 -0400

    Correct pause and unpause clicks

--- rpg/rpg_chat/js/rpg_chat.js
+++ rpg/rpg_chat/js/rpg_chat.js
@@ -23,28 +23,46 @@ Drupal.behaviors.rpgChat = {
   // Once, on page load, add the pause button to the interface.
   $('document').ready(function() {
 
-    // On page load, inject the buttons into place in the DOM.
-    if (!$('.rpg-chat-node #edit-pause').length) {
+    // If we already have the buttons, don't do anything.
+    if (!$('#edit-pause').length) {
+      // On page load, inject the buttons into place in the DOM.
       $('.rpg-chat-node #edit-actions').prepend('<input type="button" id="edit-pause" class="toggle-rpg-chat-pause form-submit" value="Pause">');
       $('.rpg-chat-node #edit-0').before('<input type="button" id="edit-2" class="toggle-rpg-chat-pause form-submit" value="Pause">');
       $('.toggle-rpg-chat-pause').css('font-weight', '').css('color', 'graytext');
+
+      // And the listener only after we have the button in place.
+      $('.toggle-rpg-chat-pause').click(function() {
+        if (paused == false) {
+          paused = true;
+          $('.toggle-rpg-chat-pause').css('font-weight', 'bold').css('color', 'red');
+        }
+        else {
+          paused = false;
+          $('.toggle-rpg-chat-pause').css('font-weight', '').css('color', 'graytext');
+          $("#edit-refresh-chat").triggerHandler("click");
+        }
+        // Otherwise do nothing.
+        return false;
+      });
+
+      // Listen for submits, bail out as needed.
+      $('form.node-form, form.comment-form').submit(function() {
+        if (in_ajax == true) {
+          return false;
+        }
+      });
+
+      // Listen for clicks, bail out as needed.
+      $('a').click(function() {
+        if (in_ajax == true) {
+          return false;
+        }
+      });
     }
 
-    // And the listener only after we have the button in place.
-    // When clicking the comment post, empty the post out.
-    $('.toggle-rpg-chat-pause').click(function() {
-      if (paused == false) {
-        paused = true;
-        $('.toggle-rpg-chat-pause').css('font-weight', 'bold').css('color', 'red');
-      }
-      else {
-        paused = false;
-        $('.toggle-rpg-chat-pause').css('font-weight', '').css('color', 'graytext');
-        $("#edit-refresh-chat").triggerHandler("click");
-      }
-      // Otherwise do nothing.
-      return false;
-    });
+    // On page load, start the chat running.
+    // On each ajax reload (as triggered by refreshChat) the ready is re-triggered.
+    timer = setTimeout(refreshChat, refreshRate * 1000);
 
   });
 
@@ -58,33 +76,12 @@ Drupal.behaviors.rpgChat = {
     $('form.node-form #edit-1, form.comment-form #edit-1, form.node-form #edit-submit, form.comment-form #edit-submit').css('color', 'black');
   });
 
-  // Listen for submits, bail out as needed.
-  $('form.node-form, form.comment-form').submit(function() {
-    if (in_ajax == true) {
-      return false;
-    }
-  });
-
   // Prevent anchor clicks while in an AJAX event.
   $('a').ajaxStart(function(){
     in_ajax = true;
   })
   .ajaxStop(function() {
     in_ajax = false;
-  });
-
-  // Listen for clicks, bail out as needed.
-  $(this).click(function() {
-    if (in_ajax == true) {
-      return false;
-    }
-  });
-
-  // On page load, start the chat running.
-  // Run only the one time to avoid ajax triggering multiple loads.
-  // On each ajax reload (as triggered by refreshChat) the ready is re-triggered.
-  $('document').ready(function() {
-    timer = setTimeout(refreshChat, refreshRate * 1000);
   });
 
   })(jQuery); }
