c83219cc9233ea5fda7592f000e87bcc5efce04a
Bastlynn <bastlynn@gmail.com>
2016-01-24 18:18:08 -0500

    nulls

--- vaxia/vaxia_chatrooms/js/vaxiaPullouts.js
+++ vaxia/vaxia_chatrooms/js/vaxiaPullouts.js
@@ -83,11 +83,11 @@ Drupal.behaviors.doPullouts = {
 
           // Copy weather and moon back into display.
           var weather = $('.weather-pic').html();
-          if (weather.length == 0) {
+          if (weather == null || weather.length == 0) {
 	        weather = '';
 	      }
           var moon = $('.moon-block').html();
-          if (moon.length == 0) {
+          if (moon == null || moon.length == 0) {
 	        moon = '';
 	      }
           var rpg_weather = '' +
