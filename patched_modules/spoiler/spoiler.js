1c0924b0aa8b1eddab13fe66f746c90aacb9d31f
Bastlynn <bastlynn@gmail.com>
2016-08-15 16:04:21 -0400

    Patched spoiler module in place

--- patched_modules/spoiler/spoiler.js
+++ patched_modules/spoiler/spoiler.js
@@ -2,23 +2,29 @@
 
 Drupal.behaviors.spoiler = {
   attach: function() {
-    $('.spoiler')
-      .addClass('spoiler-js')
-      .removeClass('spoiler')
-      .children('.spoiler-content')
-      .hide()
-      .siblings('.spoiler-warning')
-      .html(Drupal.t('<span class="spoiler-button" title="Click to view"><span>Show</span><span class="spoiler-button-hide">Hide</span> spoiler</span>'))
-      .children('.spoiler-button')
-      .click(function() {
-        $(this)
-        .parent()
-        .siblings('.spoiler-content')
-        .toggle('normal');
-        $('span', this).toggle();
-      })
-      .children('span.spoiler-button-hide')
-      .hide();
+    $('.spoiler').each(function(index) {
+      var spoiler_label = $(this).children('.spoiler-warning').attr('show');
+      $(this)
+        .addClass('spoiler-js')
+        .removeClass('spoiler')
+        .children('.spoiler-content')
+        .hide()
+        .siblings('.spoiler-warning')
+        .html(Drupal.t('<span class="spoiler-button" title="Click to view"><span>Show</span> ' +
+          '<span class="spoiler-button-hide">Hide</span> ' +
+          spoiler_label  +
+          ' </span>'))
+        .children('.spoiler-button')
+        .click(function() {
+          $(this)
+          .parent()
+          .siblings('.spoiler-content')
+          .toggle('normal');
+          $('span', this).toggle();
+        })
+        .children('span.spoiler-button-hide')
+        .hide();
+    });
   }
 };
 }(jQuery));
