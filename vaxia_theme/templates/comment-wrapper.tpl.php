697da550ebde5779a2c985c1efc43ecd2ec9359c
Bastlynn <bastlynn@gmail.com>
2017-01-30 22:28:47 -0500

    mimic bartik comment tmplate

--- vaxia_theme/templates/comment-wrapper.tpl.php
+++ vaxia_theme/templates/comment-wrapper.tpl.php
@@ -34,27 +34,24 @@
  * @see theme_comment_wrapper()
  */
 
-// Render the comments and form first to see if we need headings.
-$comments = render($content['comments']);
-$comment_form = render($content['comment_form']);
 ?>
-<section class="vaxia-theme comments <?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($comments && $node->type != 'forum'): ?>
+<section id="comments" class="vaxia-theme comments <?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if ($content['comments'] && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
     <h2 class="title"><?php print t('Comments'); ?></h2>
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
-  <?php if ($comment_form  && $node->type != 'forum'): ?>
+  <?php if ($content['comment_form']  && $node->type != 'forum'): ?>
     <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
-    <?php print $comment_form; ?>
+    <?php print render($content['comment_form']); ?>
   <?php endif; ?>
 
-  <?php print $comments; ?>
+  <?php print render($content['comments']); ?>
 
-  <?php if ($comment_form  && $node->type == 'forum'): ?>
+  <?php if ($content['comment_form']  && $node->type == 'forum'): ?>
     <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
-    <?php print $comment_form; ?>
+    <?php print render($content['comment_form']); ?>
   <?php endif; ?>
 
 </section>
