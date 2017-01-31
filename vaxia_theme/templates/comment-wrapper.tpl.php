ca17ac8dee1a3503e9ab307f5c4ed57f1b049181
Bastlynn <bastlynn@gmail.com>
2017-01-30 22:05:55 -0500

    slight wrapper reorg

--- vaxia_theme/templates/comment-wrapper.tpl.php
+++ vaxia_theme/templates/comment-wrapper.tpl.php
@@ -39,11 +39,11 @@ $comments = render($content['comments']);
 $comment_form = render($content['comment_form']);
 ?>
 <section class="vaxia-theme comments <?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
   <?php if ($comments && $node->type != 'forum'): ?>
+    <?php print render($title_prefix); ?>
     <h2 class="title"><?php print t('Comments'); ?></h2>
+    <?php print render($title_suffix); ?>
   <?php endif; ?>
-  <?php print render($title_suffix); ?>
 
   <?php if ($comment_form  && $node->type != 'forum'): ?>
     <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
